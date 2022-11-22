c3d73984d5bd1f8e3d9811714b912da55893c038
joeygarber <joseph.z.garber@gmail.com>
2022-11-22 18:49:06 -0500

    Added icons to badges page

--- src/routes/badges/badges.jsx
+++ src/routes/badges/badges.jsx
@@ -1,17 +1,23 @@
 export default function Badges () {
   return (
     <>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <div className='center'>
         <div className='badges'>
           <h1 className='title'>Badges</h1>
-          <h3 className='subheading'>The Nature Badge</h3>
+          <h3 className='subheading-badge'>The Nature Badge</h3>
+          <span class="material-symbols-outlined">park</span>
           <ol>
             <li>Touch grass</li>
             <li>Crunch a crunchy leaf</li>
             <li>Share a wilderness survival tip with the group</li>
             <li>Contemplate human nature</li>
           </ol>
-          <h3 className='subheading'>The Friendship Badge</h3>
+          <h3 className='subheading-badge'>The Friendship Badge</h3>
+          <span class="friendship material-symbols-outlined">sentiment_satisfied</span>
           <p>Scouts must complete <strong>3</strong> of the following activities:</p>
           <ol>
             <li>Write a poem about a fellow scout</li>
@@ -20,14 +26,16 @@ export default function Badges () {
             <li>Create a special handshake with one of your fellow scouts</li>
             <li>Share your greatest fear with the group</li>
           </ol>
-          <h3 className='subheading'>The Botany Badge</h3>
+          <h3 className='subheading-badge'>The Botany Badge</h3>
+          <span class="material-symbols-outlined">local_florist</span>
           <ol>
             <li>Identify a plant</li>
             <li>Eat a fruit or vegetable</li>
             <li>Draw a picture of a plant</li>
             <li>Name your favorite part of the plant cell</li>
           </ol>
-          <h3 className='subheading'>The Ally Badge</h3>
+          <h3 className='subheading-badge'>The Ally Badge</h3>
+          <span class="material-symbols-outlined">looks</span>
           <ol>
             <li>Give a queer person a snack</li>
             <li>Use a gay slang term correctly in a sentence</li>
--- src/routes/header/header.jsx
+++ src/routes/header/header.jsx
@@ -3,14 +3,21 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default function Header () {
   return (
-    <Navbar expand='md'>
-      <Navbar.Brand>
-        <Link style={{ textDecoration: 'none' }} to='/'>Leaf Scouts</Link>
-      </Navbar.Brand>
-      <NavLink className='navLink' to='/members'>Members</NavLink>
-      <NavLink className='navLink' to='/badges'>Badges</NavLink>
-      <NavLink className='navLink' to='/pledge-and-values'>Pledge and Values</NavLink>
-    </Navbar>
+    <>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      <Navbar expand='md'>
+      <span id='logo' className="material-symbols-outlined">spa</span>
+        <Navbar.Brand>
+          <Link style={{ textDecoration: 'none' }} to='/'>Leaf Scouts</Link>
+        </Navbar.Brand>
+        <NavLink className='navLink' to='/members'>Members</NavLink>
+        <NavLink className='navLink' to='/badges'>Badges</NavLink>
+        <NavLink className='navLink' to='/pledge-and-values'>Pledge and Values</NavLink>
+      </Navbar>
+    </>
+
+    
+
   )
   
 }
