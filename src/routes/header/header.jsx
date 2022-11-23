66d4b12efa35f67a9305c59831e88daf80874d60
joeygarber <joseph.z.garber@gmail.com>
2022-11-22 21:10:00 -0500

    Responsive design tweaks

--- src/routes/header/header.jsx
+++ src/routes/header/header.jsx
@@ -8,7 +8,6 @@ export default function Header () {
     <>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <Navbar expand='lg'>
-        <Container>
           <span id='logo' className="material-symbols-outlined">spa</span>
           <Navbar.Brand>
             <Link style={{ textDecoration: 'none' }} to='/'>Leaf Scouts</Link>
@@ -16,12 +15,11 @@ export default function Header () {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
+              <NavLink className='navLink' to='/pledge-and-values'>Pledge and Values</NavLink>
               <NavLink className='navLink' to='/members'>Members</NavLink>
               <NavLink className='navLink' to='/badges'>Badges</NavLink>
-              <NavLink className='navLink' to='/pledge-and-values'>Pledge and Values</NavLink>
             </Nav>
           </Navbar.Collapse>
-        </Container>
       </Navbar>
     </>
   )
--- src/routes/pledge-and-values/pledgeandvalues.jsx
+++ src/routes/pledge-and-values/pledgeandvalues.jsx
@@ -3,22 +3,22 @@ export default function PledgeAndValues () {
     <>
       <div className='pledge'>
         <h1 className='title'>The Leaf Scout Pledge</h1>
-        <text>
+        <p>
           <span>I pledge to be a leaf</span>
           <span>To always tree-t others kindly</span>
           <span>To never stop branching out</span>
           <span>To stay rooted in the 4 scouting values</span>
           <span>And to never leaf my fellow scouts behind</span>
-        </text>
+        </p>
       </div>
       <div className='values'>
         <h1 className='title'>The 4 Scouting Values</h1>
-        <text>
-          <span><span className='bold'>L</span>aughter</span>
-          <span><span className='bold'>E</span>mpathy</span>
-          <span><span className='bold'>A</span>ccepting the Passage of Time</span>
-          <span><span className='bold'>F</span>riendship</span>
-        </text>
+        <p>
+          <span><strong>L</strong>aughter</span>
+          <span><strong>E</strong>mpathy</span>
+          <span><strong>A</strong>ccepting the Passage of Time</span>
+          <span><strong>F</strong>riendship</span>
+        </p>
       </div>
     </>
   )
