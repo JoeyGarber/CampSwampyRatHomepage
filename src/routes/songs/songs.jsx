06b96c5189b6cba2dd6b1198c3d845ce65dc7b89
joeygarber <joseph.z.garber@gmail.com>
2022-11-24 12:59:57 -0500

    Styled Spotify embed

--- src/routes/songs/songs.jsx
+++ src/routes/songs/songs.jsx
@@ -1,8 +1,10 @@
 export default function Songs () {
   return (
     <>
-    <h1 className="title center">Camp Songs</h1>
-    <iframe style={{"borderRadius": "12px"}} src="https://open.spotify.com/embed/playlist/41IJktFXoX7x9NABcLA4kN?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <h1 className="title center">Camp Songs</h1>
+      <div id="spotifyembedcontainer">
+        <iframe style={{"borderRadius": "12px"}} src="https://open.spotify.com/embed/playlist/41IJktFXoX7x9NABcLA4kN?utm_source=generator&theme=0" width="80%" height="380" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      </div>
     </>
   )
 }
